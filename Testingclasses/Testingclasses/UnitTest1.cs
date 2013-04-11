7b861fe7098e76ad3d4fb51735bd3c012e2576d5
Otto Enmanuel <ottoevargas@gmail.com>
2013-04-10 21:15:23 -0400

    Start Unit Testing

---
+++ Testingclasses/Testingclasses/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BowlingScore;
+
+namespace Testingclasses
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ScoreDetectionok()
+        {
+            ScoreDetection sd = new ScoreDetection();
+            Player p1 = new Player();
+            Player p2 = new Player();
+            sd.Player1 = p1;
+            sd.Player2 = p2;
+
+            Assert.IsNotNull(p1);
+            Assert.IsNotNull(p2);
+        }
+    }
+}
